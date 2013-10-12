8db2bb6e4051ff651aa62dabb5219ed1eb771711
Johan Hammar <johan.hammar@gmail.com>
2013-10-12 10:46:35 +0200

    Updated Nightcode to 0.1.2

--- nightcode.rb
+++ nightcode.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nightcode < Formula
   homepage 'https://nightcode.info/'
-  url 'https://github.com/oakes/Nightcode/releases/download/0.1.1/nightcode-0.1.1-standalone.jar'
-  sha1 '7556c66e16efb9b0a1d1fa338cb1cc357979e658'
+  url 'https://github.com/oakes/Nightcode/releases/download/0.1.2/nightcode-0.1.2-standalone.jar'
+  sha1 '4466a6bda063396b4b00787077f21353b1a88d5b'
 
   def install
     libexec.install "nightcode-#{version}-standalone.jar"
